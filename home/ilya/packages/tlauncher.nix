cfd11f46fa0dffae1d90fb4b4d51ba4a4f3246d9
Gadjib <ilya@andrianov.ru>
2026-08-20 04:26:51 +0300

    Update verified TLauncher seed

--- home/ilya/packages/tlauncher.nix
+++ home/ilya/packages/tlauncher.nix
@@ -10,11 +10,11 @@
 
 stdenvNoCC.mkDerivation {
   pname = "tlauncher";
-  version = "2026-01-06";
+  version = "2026-08-14";
 
   src = fetchurl {
     url = "https://tlauncher.org/jar";
-    hash = "sha256-xht2qWiXbOi6ezHziEcSVCxN6BPKDG8yQZSvULnLRW8=";
+    hash = "sha256-93A2WHQvl5BSkKGUTEu2bfSY5GIBZBNAyTVP8ZZ0be0=";
   };
 
   nativeBuildInputs = [
@@ -25,48 +25,48 @@ stdenvNoCC.mkDerivation {
   dontUnpack = true;
 
   installPhase = ''
-    runHook preInstall
+        runHook preInstall
 
-    mkdir -p "$out/share/tlauncher" "$out/bin" "$out/share/applications"
-    unzip -j "$src" TLauncher.jar -d "$out/share/tlauncher"
+        mkdir -p "$out/share/tlauncher" "$out/bin" "$out/share/applications"
+        unzip -j "$src" TLauncher.jar -d "$out/share/tlauncher"
 
-    cat > "$out/bin/tlauncher" <<EOF
-#!${runtimeShell}
-set -euo pipefail
+        cat > "$out/bin/tlauncher" <<EOF
+    #!${runtimeShell}
+    set -euo pipefail
 
-runtime_dir="\''${XDG_DATA_HOME:-\$HOME/.local/share}/tlauncher"
-runtime_jar="\$runtime_dir/TLauncher.jar"
-store_jar="$out/share/tlauncher/TLauncher.jar"
+    runtime_dir="\''${XDG_DATA_HOME:-\$HOME/.local/share}/tlauncher"
+    runtime_jar="\$runtime_dir/TLauncher.jar"
+    store_jar="$out/share/tlauncher/TLauncher.jar"
 
-mkdir -p "\$runtime_dir"
-if [[ ! -f "\$runtime_jar" ]]; then
-  cp "\$store_jar" "\$runtime_jar"
-  chmod u+w "\$runtime_jar"
-fi
+    mkdir -p "\$runtime_dir"
+    if [[ ! -f "\$runtime_jar" ]]; then
+      cp "\$store_jar" "\$runtime_jar"
+      chmod u+w "\$runtime_jar"
+    fi
 
-exec ${jre}/bin/java -Dfile.encoding=UTF-8 -jar "\$runtime_jar" "\$@"
-EOF
-    chmod +x "$out/bin/tlauncher"
+    exec ${jre}/bin/java -Dfile.encoding=UTF-8 -jar "\$runtime_jar" "\$@"
+    EOF
+        chmod +x "$out/bin/tlauncher"
 
-    cat > "$out/share/applications/tlauncher.desktop" <<'EOF'
-[Desktop Entry]
-Type=Application
-Version=1.5
-Name=TLauncher
-GenericName=Minecraft launcher
-Comment=TLauncher Minecraft launcher
-Exec=tlauncher
-Icon=applications-games
-Terminal=false
-StartupNotify=true
-StartupWMClass=TLauncher
-Categories=Game;
-Keywords=Minecraft;Launcher;Game;
-EOF
+        cat > "$out/share/applications/tlauncher.desktop" <<'EOF'
+    [Desktop Entry]
+    Type=Application
+    Version=1.5
+    Name=TLauncher
+    GenericName=Minecraft launcher
+    Comment=TLauncher Minecraft launcher
+    Exec=tlauncher
+    Icon=applications-games
+    Terminal=false
+    StartupNotify=true
+    StartupWMClass=TLauncher
+    Categories=Game;
+    Keywords=Minecraft;Launcher;Game;
+    EOF
 
-    desktop-file-validate "$out/share/applications/tlauncher.desktop"
+        desktop-file-validate "$out/share/applications/tlauncher.desktop"
 
-    runHook postInstall
+        runHook postInstall
   '';
 
   meta = {
